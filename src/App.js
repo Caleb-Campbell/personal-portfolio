dd6548dc830a2e358812a75fbf4dc72411ad45bd
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-10-20 15:15:42 -0600

    Added functionality to make the navbar switch to title

--- src/App.js
+++ src/App.js
@@ -13,25 +13,15 @@ const mainBody = styled.div`
 `
 
 function App() {
-const [dark, setDark] = useState(false)
 
-
-const handleDarkButton = () => {
-  setDark(!dark)
-  if(dark) {
-    document.body.classList.toggle('dark-mode')
-    document.body.childNodes.toggle('light-text')
-  }
-}
 
   return (
     <div className="App" >
       <div className='darkModeWrap' >
-      <button onClick={handleDarkButton} className='darkMode'>Dark</button>
       </div>
       <Routes>
-        <Route path='/' dark={dark} element={<Home />} />
-        <Route path='/about' dark={dark} element={<About />} />
+        <Route path='/'  element={<Home />} />
+        <Route path='/about'  element={<About />} />
       </Routes>
 
     </div>
