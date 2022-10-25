33d0f7b866813d7f1a59ff7aa67c64f973957883
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-10-24 21:01:56 -0600

    animated some stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ const Home = (props) => {
     return (
       <div className="home">
         <NavBar />
+        <h1 className="WebTitle">Full <br/> Stack <br/> <span className="WebTitle-big">Web Developer</span></h1>
         <section className="spacer"></section>
         <h3 className="Subtitle">See My Technical Skills</h3>
         <Skills techSkills={props.skills} />
